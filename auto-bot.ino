af13e79d53ddb8a2c42bf1f031f76a82be12d57c
Matias Micheltorena <matias.micheltorena@gmail.com>
2016-02-14 16:34:01 -0300

    added simple brake and forward lights output

--- auto-bot.ino
+++ auto-bot.ino
@@ -1,7 +1,7 @@
 /*
   Auto-Bot Arduino based Bot.
   Arduino MEGA + L293D Shield + MINI DC GearBox X4 + HC-05 Bluetooth
-  
+
   Author: Matias Micheltorena (@matiargs)
   Version: 1.0.0
 */
@@ -18,61 +18,75 @@ AF_DCMotor Motor4(4);
 //Define command variable
 char cmd = '0';
 
+//Define led pin
+const int forwardLed = 22;
+const int brakeLed = 23;
+
+
 void setup() {
 
   //Open serial communications at 9600 baudrate for second serial interface
-  Serial1.begin(9600); 
+  Serial1.begin(9600);
 
   //Define motor speed @255
   Motor1.setSpeed(255);
   Motor2.setSpeed(255);
   Motor3.setSpeed(255);
-  Motor4.setSpeed(255);  
+  Motor4.setSpeed(255);
   
 }
 
 void loop() {
 
-  //Verify connection available
-  if ( Serial1.available() ) 
-  {  
-      //Read incoming char    
-      cmd = Serial1.read();                           
+  //Verify if bluetooth connection is available
+  if ( Serial1.available() )
+  {
+    //Read incoming char
+    cmd = Serial1.read();
   }
 
   //First attemp to control the actions of the bot, this need a little refactor.
-  
+
   //Go Fordward
-  if (ch == 'z')
-  {        
+  if (cmd == 'z')
+  {
     Motor1.run(FORWARD);
-    Motor2.run(FORWARD);                  
+    Motor2.run(FORWARD);
     Motor3.run(FORWARD);
-    Motor4.run(FORWARD);       
+    Motor4.run(FORWARD);
+
+    digitalWrite(brakeLed, LOW);
+    digitalWrite(forwardLed, HIGH); 
   }
 
   //Go Backward
-  if (ch == 'x')
-  {          
+  if (cmd == 'x')
+  {
     Motor1.run(BACKWARD);
-    Motor2.run(BACKWARD);     
+    Motor2.run(BACKWARD);
     Motor3.run(BACKWARD);
-    Motor4.run(BACKWARD);   
+    Motor4.run(BACKWARD);
+
+    digitalWrite(forwardLed, LOW);
+    digitalWrite(brakeLed, HIGH);
   }
 
   //Stop - Brake
-  if (ch == 'p')
-  {       
+  if (cmd == 'p')
+  {
     Motor1.run(RELEASE);
     Motor2.run(RELEASE);
     Motor3.run(RELEASE);
     Motor4.run(RELEASE);
-    
+        
+    digitalWrite(forwardLed, LOW);
+    digitalWrite(brakeLed, HIGH);
+    delay(100);
+    digitalWrite(brakeLed, LOW);
   }
-  
-  /*Beacuse this is running inside the loop function, 
-   we need to set the command to '0' to prevent continous invoking the function run */
-   ch = '0'; 
-  
-}  
+
+  /*Beacuse this is running inside the loop function,
+    we need to set the command to '0' to prevent continous invoking the function run */
+  cmd = '0';
+}
 
